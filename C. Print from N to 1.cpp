8a996643bc7b8e9533f5a6c86a233a3226b53028
Moustafa270 <ald7ee72672005@gmail.com>
2026-02-28 17:31:10 +0200

    Add program to print numbers from N to 1

---
+++ C. Print from N to 1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void print(int x){
+
+   if (x == 0)
+   {
+    return;
+   }
+    if (x == 1) {
+        cout << x;
+    } else {
+        cout << x << " " <<endl;
+    }
+
+    print(x-1);
+
+}
+
+
+int main(){
+    int x;
+    cin>>x;
+    print(x);
+return 0;
+}
