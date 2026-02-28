7f50b797ca8534d2be02b6f7e73a064195f4a92c
Moustafa270 <ald7ee72672005@gmail.com>
2026-02-28 17:19:09 +0200

    Add recursive function to print numbers from 1 to N

---
+++ B. Print from 1 to N.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void print(int x){
+
+   if (x == 0)
+   {
+    return;
+   }
+
+   print(x-1);
+
+    cout<<x<<endl;
+}
+
+
+int main(){
+    int x;
+    cin>>x;
+    print(x);
+return 0;
+}
