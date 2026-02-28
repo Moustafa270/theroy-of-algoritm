1c7577a9f3f87369c0fbafe8ea1660214db4d68b
Moustafa270 <ald7ee72672005@gmail.com>
2026-02-28 17:45:59 +0200

    Implement factorial function in J. Factorial.cpp

---
+++ J. Factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+long long factorial (int x){
+    if (x <=1)
+    {
+        return 1;
+    }
+
+    return x * factorial(x - 1);
+
+}
+
+int main() {
+
+   int x;
+   cin>>x;
+   cout<<factorial(x);
+
+    return 0;
+}
